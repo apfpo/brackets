b1722130058398883d6028eb4f780a08fda74702
apfpo <apfpo2001@gmail.com>
2020-10-20 13:07:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const map = new Map(bracketsConfig);
+    const stack = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === map.get(stack[stack.length - 1])) stack.pop();
+        else stack.push(str[i]);
+    }
+    return stack.length === 0;
 }
